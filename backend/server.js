5f81dd4360149a022df9436e2927ec3e16a3c355
Shagun Singh <yashmit36@gmail.com>
2025-01-09 15:25:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express(); //This initializes a new Express application instance, wh
 //nodemon package for restarting the server whenever new changes are made.
 
 app.use(express.json()); //allows us to accept json data in req.body
-app.use(cors());
+app.use(cors({origin:'*'}));
 const __dirname = path.resolve();
 app.use("/api/products", productRoutes); //handler function of routes are separated
 
