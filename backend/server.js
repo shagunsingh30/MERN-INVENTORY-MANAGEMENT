a896f30e220ce0a4607467da60c094d2302d19b1
Shagun Singh <yashmit36@gmail.com>
2025-01-09 16:03:03 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,14 +14,7 @@ const app = express(); //This initializes a new Express application instance, wh
 //nodemon package for restarting the server whenever new changes are made.
 
 app.use(express.json()); //allows us to accept json data in req.body
-app.use(
-  cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://stock-nest-back-end.onrender.com"
-        : "*", // Wildcard for local development
-  })
-);
+app.use(cors());
 const __dirname = path.resolve();
 app.use("/api/products", productRoutes); //handler function of routes are separated
 
